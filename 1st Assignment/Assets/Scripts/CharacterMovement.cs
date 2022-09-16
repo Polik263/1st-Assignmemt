c89eb0b7ed0c6acaebd663edceb13f1b73505c40
Polik263 <a.dominik2001@interia.eu>
2022-09-16 16:31:13 +0200

    1st Assignment

---
+++ 1st Assignment/Assets/Scripts/CharacterMovement.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour
+{
+    [SerializeField] private PlayerTurn playerTurn;
+    [SerializeField] private Rigidbody characterBody;
+    [SerializeField] private float speed = 2f;
+
+    void Update()
+    {
+        if (playerTurn.IsPlayerTurn())
+        {
+            if (Input.GetAxis("Horizontal") != 0)
+            {
+                Vector3 direction = Vector3.right * Input.GetAxis("Horizontal");
+                transform.Translate(direction * speed * Time.deltaTime, Space.World);
+            }
+
+            if (Input.GetAxis("Vertical") != 0)
+            {
+                Vector3 direction = Vector3.forward * Input.GetAxis("Vertical");
+                transform.Translate(direction * speed * Time.deltaTime, Space.World);
+            }
+            if (Input.GetKeyDown(KeyCode.Space) && IsTouchingFloor())
+            {
+                Jump();
+            }
+        }
+    }
+    
+
+
+    private void Jump()
+    {
+        
+        characterBody.AddForce(Vector3.up * 200f);
+    }
+    private bool IsTouchingFloor()
+    {
+        Vector3 position = transform.position + Vector3.down * 1f;
+        RaycastHit hit;
+        return Physics.SphereCast(transform.position, 0.15f, Vector3.down, out hit, 1f);
+    }
+    
+}
