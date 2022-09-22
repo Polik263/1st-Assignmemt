b26bb7b34571918f1e9eef1ddced452a0c136aab
Polik263 <a.dominik2001@interia.eu>
2022-09-22 12:46:09 +0200

    1st Assignment Animations added

---
+++ 1st Assignment/Assets/Animations/WalkingAnimation.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WalkingAnimation : MonoBehaviour
+{
+    [SerializeField] private Animator animator;
+    [SerializeField] private PlayerTurn playerTurn;
+
+
+    private void Update()
+    {
+        bool IsPlayerTurn = playerTurn.IsPlayerTurn();
+        if (IsPlayerTurn)
+        {
+
+            bool isWalking = Input.GetAxis("Horizontal") != 0 || Input.GetAxis("Vertical") != 0;
+
+            animator.SetBool("isWalking", isWalking);
+        }
+
+    }
+}
